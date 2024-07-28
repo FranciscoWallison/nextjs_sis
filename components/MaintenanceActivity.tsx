6e76327607e6e9ba157535019033b718422b7bfd
Francisco Wallison <franciscowallison@gmail.com>
2024-07-27 22:32:02 -0300

    iniciando logica de perguntas

--- components/MaintenanceActivity.tsx
+++ components/MaintenanceActivity.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardContent, Typography, Box } from '@mui/material';
+import React from "react";
+import { Card, CardContent, Typography, Box } from "@mui/material";
 
 interface ResponsibleInfo {
   nome: string;
@@ -20,7 +20,22 @@ interface MaintenanceActivityProps {
   activity: Activity;
 }
 
-const MaintenanceActivity: React.FC<MaintenanceActivityProps> = ({ activity }) => {
+const MaintenanceActivity: React.FC<MaintenanceActivityProps> = ({
+  activity,
+}) => {
+  function formatDate(input) {
+    if (input === undefined) {
+      return;
+    }
+
+    const datePart = input.match(/\d+/g),
+      year = datePart[0].substring(2), // get only two digits
+      month = datePart[1],
+      day = datePart[2];
+
+    return day + "/" + month + "/" + year;
+  }
+
   return (
     <Card sx={{ mb: 2 }}>
       <CardContent>
@@ -39,14 +54,34 @@ const MaintenanceActivity: React.FC<MaintenanceActivityProps> = ({ activity }) =
         <Typography variant="body2">
           <strong>Obrigatório:</strong> {activity.obrigatorio}
         </Typography>
+
         <Box mt={2}>
+          <Typography variant="body2">
+            <strong>Feitos:</strong>
+          </Typography>
+          <Typography variant="body2">
+            Data: {formatDate(activity.data)}
+          </Typography>
+          {activity.nao_feito && (
+            <Typography variant="body2">
+              Não foi feito: {activity.nao_feito ? "sim" : "não"}
+            </Typography>
+          )}
+          {activity.nao_lembro && (
+            <Typography variant="body2">
+              Não Lembro: {activity.nao_lembro ? "sim" : "não"}
+            </Typography>
+          )}
+        </Box>
+
+        {/* <Box mt={2}>
           <Typography variant="body2">
             <strong>Responsável Info:</strong>
           </Typography>
           <Typography variant="body2">Nome: {activity.responsavel_info.nome}</Typography>
           <Typography variant="body2">Telefone: {activity.responsavel_info.telefone}</Typography>
           <Typography variant="body2">Email: {activity.responsavel_info.email}</Typography>
-        </Box>
+        </Box> */}
       </CardContent>
     </Card>
   );
