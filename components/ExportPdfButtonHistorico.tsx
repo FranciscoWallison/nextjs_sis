e5ddc12bb5c906a2d6ac09565d6400ec756f9619
Francisco Wallison <franciscowallison@gmail.com>
2024-12-13 20:49:42 -0300

    add imagens

--- components/ExportPdfButtonHistorico.tsx
+++ components/ExportPdfButtonHistorico.tsx
@@ -31,7 +31,7 @@ const ExportPdfButton: React.FC<ExportPdfButtonProps> = ({
     doc.setFillColor(244, 244, 244); // Cor RGB (244, 244, 244) equivalente ao var(--scrollbar-bg-color)
     doc.rect(0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), "F"); // "F" para preenchimento
     // Carregar a imagem local e adicionar ao PDF
-    const imagePath = "./logo-gmp.svg"; // Caminho para a imagem local
+    const imagePath = "/logo.png"; // Caminho para a imagem na pasta public
     const loadImageToBase64 = (url: string): Promise<string> => {
       return new Promise((resolve, reject) => {
         const img = new Image();
@@ -58,7 +58,6 @@ const ExportPdfButton: React.FC<ExportPdfButtonProps> = ({
 
     // Título do documento
     doc.setFontSize(18);
-
     
 
     // Data de geração
@@ -67,7 +66,6 @@ const ExportPdfButton: React.FC<ExportPdfButtonProps> = ({
 
     // Cabeçalhos da tabela
     const tableColumnHeaders = [
-      // "Título",
       "Periodicidade",
       "Data",
       "Blocos",
@@ -76,9 +74,9 @@ const ExportPdfButton: React.FC<ExportPdfButtonProps> = ({
 
     // Dados da tabela com validações
     const tableRows = activities.map((activity) => {
+      
       doc.text(activity.updatedFields?.titulo, 70, 20);
       return [
-      // activity.updatedFields?.titulo || "Título Indisponível",
       activity.updatedFields?.Periodicidade || "Periodicidade Indisponível",
       activity.updatedFields?.data
         ? dayjs(activity.updatedFields.data).format("DD/MM/YYYY")
